25cae16e4508367ed1018dcc3b41128db859dece
Damien Chazoule <damien.chazoule@gmail.com>
2019-03-25 14:23:30 +0100

    PWA Support

--- Front/src/main.ts
+++ Front/src/main.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './components/App.vue';
 import apolloProvider from './apollo';
 import router from './router';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
